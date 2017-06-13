413a5c1f5940c10fc8834fb303d88566d1c724f9
nicholasRenninger <nire1188@colorado.edu>
2017-06-12 23:59:47 -0600

    Added new directories

--- src/bits.c
+++ src/bits.c
@@ -26,68 +26,68 @@ editing the collection of functions in this source file.
 
 INTEGER CODING RULES:
  
-  Replace the "return" statement in each function with one
-  or more lines of C code that implements the function. Your code 
-  must conform to the following style:
+	Replace the "return" statement in each function with one
+	or more lines of C code that implements the function. Your code 
+	must conform to the following style:
  
-  int Funct(arg1, arg2, ...) {
-      /* brief description of how your implementation works */
-      int var1 = Expr1;
-      ...
-      int varM = ExprM;
+	int Funct(arg1, arg2, ...) {
+			/* brief description of how your implementation works */
+			int var1 = Expr1;
+			...
+			int varM = ExprM;
 
-      varJ = ExprJ;
-      ...
-      varN = ExprN;
-      return ExprR;
-  }
+			varJ = ExprJ;
+			...
+			varN = ExprN;
+			return ExprR;
+	}
 
-  Each "Expr" is an expression using ONLY the following:
-  1. Integer constants 0 through 255 (0xFF), inclusive. You are
-      not allowed to use big constants such as 0xffffffff.
-  2. Function arguments and local variables (no global variables).
-  3. Unary integer operations ! ~
-  4. Binary integer operations & ^ | + << >>
-    
-  Some of the problems restrict the set of allowed operators even further.
-  Each "Expr" may consist of multiple operators. You are not restricted to
-  one operator per line.
-    
-  You are expressly forbidden to:
-  1. Use any control constructs such as if, do, while, for, switch, etc.
-  2. Define or use any macros.
-  3. Define any additional functions in this file.
-  4. Call any functions.
-  5. Use any other operations, such as &&, ||, -, or ?:
-  6. Use any form of casting.
-  7. Use any data type other than int.  This implies that you
-     cannot use arrays, structs, or unions.
+	Each "Expr" is an expression using ONLY the following:
+	1. Integer constants 0 through 255 (0xFF), inclusive. You are
+			not allowed to use big constants such as 0xffffffff.
+	2. Function arguments and local variables (no global variables).
+	3. Unary integer operations ! ~
+	4. Binary integer operations & ^ | + << >>
+		
+	Some of the problems restrict the set of allowed operators even further.
+	Each "Expr" may consist of multiple operators. You are not restricted to
+	one operator per line.
+		
+	You are expressly forbidden to:
+	1. Use any control constructs such as if, do, while, for, switch, etc.
+	2. Define or use any macros.
+	3. Define any additional functions in this file.
+	4. Call any functions.
+	5. Use any other operations, such as &&, ||, -, or ?:
+	6. Use any form of casting.
+	7. Use any data type other than int.  This implies that you
+		 cannot use arrays, structs, or unions.
 
  
-  You may assume that your machine:
-  1. Uses 2s complement, 32-bit representations of integers.
-  2. Performs right shifts arithmetically.
-  3. Has unpredictable behavior when shifting an integer by more
-     than the word size.
+	You may assume that your machine:
+	1. Uses 2s complement, 32-bit representations of integers.
+	2. Performs right shifts arithmetically.
+	3. Has unpredictable behavior when shifting an integer by more
+		 than the word size.
 
 EXAMPLES OF ACCEPTABLE CODING STYLE:
-  /*
-   * pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
-   */
-  int pow2plus1(int x) {
-     /* exploit ability of shifts to compute powers of 2 */
-     return (1 << x) + 1;
-  }
+	/*
+	 * pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
+	 */
+	int pow2plus1(int x) {
+		 /* exploit ability of shifts to compute powers of 2 */
+		 return (1 << x) + 1;
+	}
 
-  /*
-   * pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
-   */
-  int pow2plus4(int x) {
-     /* exploit ability of shifts to compute powers of 2 */
-     int result = (1 << x);
-     result += 4;
-     return result;
-  }
+	/*
+	 * pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
+	 */
+	int pow2plus4(int x) {
+		 /* exploit ability of shifts to compute powers of 2 */
+		 int result = (1 << x);
+		 result += 4;
+		 return result;
+	}
 
 FLOATING POINT CODING RULES
 
@@ -97,28 +97,28 @@ conditional control.  You are allowed to use both ints and unsigneds.
 You can use arbitrary integer and unsigned constants.
 
 You are expressly forbidden to:
-  1. Define or use any macros.
-  2. Define any additional functions in this file.
-  3. Call any functions.
-  4. Use any form of casting.
-  5. Use any data type other than int or unsigned.  This means that you
-     cannot use arrays, structs, or unions.
-  6. Use any floating point data types, operations, or constants.
+	1. Define or use any macros.
+	2. Define any additional functions in this file.
+	3. Call any functions.
+	4. Use any form of casting.
+	5. Use any data type other than int or unsigned.  This means that you
+		 cannot use arrays, structs, or unions.
+	6. Use any floating point data types, operations, or constants.
 
 
 NOTES:
-  1. Use the dlc (data lab checker) compiler (described in the handout) to 
-     check the legality of your solutions.
-  2. Each function has a maximum number of operators (! ~ & ^ | + << >>)
-     that you are allowed to use for your implementation of the function. 
-     The max operator count is checked by dlc. Note that '=' is not 
-     counted; you may use as many of these as you want without penalty.
-  3. Use the btest test harness to check your functions for correctness.
-  4. Use the BDD checker to formally verify your functions
-  5. The maximum number of ops for each function is given in the
-     header comment for each function. If there are any inconsistencies 
-     between the maximum ops in the writeup and in this file, consider
-     this file the authoritative source.
+	1. Use the dlc (data lab checker) compiler (described in the handout) to 
+		 check the legality of your solutions.
+	2. Each function has a maximum number of operators (! ~ & ^ | + << >>)
+		 that you are allowed to use for your implementation of the function. 
+		 The max operator count is checked by dlc. Note that '=' is not 
+		 counted; you may use as many of these as you want without penalty.
+	3. Use the btest test harness to check your functions for correctness.
+	4. Use the BDD checker to formally verify your functions
+	5. The maximum number of ops for each function is given in the
+		 header comment for each function. If there are any inconsistencies 
+		 between the maximum ops in the writeup and in this file, consider
+		 this file the authoritative source.
 
 /*
  * STEP 2: Modify the following functions according the coding rules.
@@ -133,40 +133,40 @@ NOTES:
 
 #endif
 /* Copyright (C) 1991-2016 Free Software Foundation, Inc.
-   This file is part of the GNU C Library.
+	 This file is part of the GNU C Library.
 
-   The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
+	 The GNU C Library is free software; you can redistribute it and/or
+	 modify it under the terms of the GNU Lesser General Public
+	 License as published by the Free Software Foundation; either
+	 version 2.1 of the License, or (at your option) any later version.
 
-   The GNU C Library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
+	 The GNU C Library is distributed in the hope that it will be useful,
+	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	 Lesser General Public License for more details.
 
-   You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; if not, see
-   <http://www.gnu.org/licenses/>.  */
+	 You should have received a copy of the GNU Lesser General Public
+	 License along with the GNU C Library; if not, see
+	 <http://www.gnu.org/licenses/>.  */
 
 /* This header is separate from features.h so that the compiler can
-   include it implicitly at the start of every compilation.  It must
-   not itself include <features.h> or any other header that includes
-   <features.h> because the implicit include comes before any feature
-   test macros that may be defined in a source file before it first
-   explicitly includes a system header.  GCC knows the name of this
-   header in order to preinclude it.  */
+	 include it implicitly at the start of every compilation.  It must
+	 not itself include <features.h> or any other header that includes
+	 <features.h> because the implicit include comes before any feature
+	 test macros that may be defined in a source file before it first
+	 explicitly includes a system header.  GCC knows the name of this
+	 header in order to preinclude it.  */
 
 /* glibc's intent is to support the IEC 559 math functionality, real
-   and complex.  If the GCC (4.9 and later) predefined macros
-   specifying compiler intent are available, use them to determine
-   whether the overall intent is to support these features; otherwise,
-   presume an older compiler has intent to support these features and
-   define these macros by default.  */
+	 and complex.  If the GCC (4.9 and later) predefined macros
+	 specifying compiler intent are available, use them to determine
+	 whether the overall intent is to support these features; otherwise,
+	 presume an older compiler has intent to support these features and
+	 define these macros by default.  */
 
 /* wchar_t uses Unicode 8.0.0.  Version 8.0 of the Unicode Standard is
-   synchronized with ISO/IEC 10646:2014, plus Amendment 1 (published
-   2015-05-15).  */
+	 synchronized with ISO/IEC 10646:2014, plus Amendment 1 (published
+	 2015-05-15).  */
 
 /* We do not support C11 <threads.h>.  */
 
@@ -179,7 +179,8 @@ NOTES:
  *   Rating: 1
  */
 int bitAnd(int x, int y) {
-  return 2;
+	/* Use De Morgan's Laws: A & B = ~(~A | ~B)*/
+	return ~(~x | ~y);
 }
 
 /* 
@@ -191,7 +192,7 @@ int bitAnd(int x, int y) {
  *   Rating: 2
  */
 int getByte(int x, int n) {
-  return 2;
+	return 2;
 }
 
 /* 
@@ -203,7 +204,7 @@ int getByte(int x, int n) {
  *   Rating: 3 
  */
 int logicalShift(int x, int n) {
-  return 2;
+	return 2;
 }
 
 /*
@@ -214,7 +215,7 @@ int logicalShift(int x, int n) {
  *   Rating: 4
  */
 int bitCount(int x) {
-  return 2;
+	return 2;
 }
 
 /* 
@@ -225,7 +226,7 @@ int bitCount(int x) {
  *   Rating: 4 
  */
 int bang(int x) {
-  return 2;
+	return 2;
 }
 
 /* 
@@ -235,7 +236,7 @@ int bang(int x) {
  *   Rating: 1
  */
 int tmin(void) {
-  return 2;
+	return 2;
 }
 
 /* 
@@ -248,7 +249,7 @@ int tmin(void) {
  *   Rating: 2
  */
 int fitsBits(int x, int n) {
-  return 2;
+	return 2;
 }
 
 /* 
@@ -260,7 +261,7 @@ int fitsBits(int x, int n) {
  *   Rating: 2
  */
 int divpwr2(int x, int n) {
-    return 2;
+		return 2;
 }
 
 /* 
@@ -271,7 +272,7 @@ int divpwr2(int x, int n) {
  *   Rating: 2
  */
 int negate(int x) {
-  return 2;
+	return 2;
 }
 
 /* 
@@ -282,7 +283,7 @@ int negate(int x) {
  *   Rating: 3
  */
 int isPositive(int x) {
-  return 2;
+	return 2;
 }
 
 /* 
@@ -293,7 +294,7 @@ int isPositive(int x) {
  *   Rating: 3
  */
 int isLessOrEqual(int x, int y) {
-  return 2;
+	return 2;
 }
 
 /*
@@ -304,7 +305,7 @@ int isLessOrEqual(int x, int y) {
  *   Rating: 4
  */
 int ilog2(int x) {
-  return 2;
+	return 2;
 }
 
 /* 
@@ -332,7 +333,7 @@ unsigned float_neg(unsigned uf) {
  *   Rating: 4
  */
 unsigned float_i2f(int x) {
-  return 2;
+	return 2;
 }
 
 /* 
@@ -347,5 +348,5 @@ unsigned float_i2f(int x) {
  *   Rating: 4
  */
 unsigned float_twice(unsigned uf) {
-  return 2;
+	return 2;
 }
